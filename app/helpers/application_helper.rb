a33c168c9219d40dde63f5d1c341f488c6ac201e
MinasMorgulNazgul <razborov-aa@yandex.ru>
2022-02-13 12:55:42 +0300

    add count question

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       asset_path 'avatar.png'
     end
   end
+
+  def count_questions(number, word1, word2, word3)
+    if (number % 100).between?(11, 14)
+      "#{number} #{word3}"
+    elsif number % 10 == 1
+      "#{number} #{word1}"
+    elsif (number % 10).between?(2,4)
+      "#{number} #{word2}"
+    else
+      "#{number} #{word3}"
+    end
+  end
 end
