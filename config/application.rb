309395f55e803ac8f0b4fcf378bfa83a577524f2
MinasMorgulNazgul <razborov-aa@yandex.ru>
2022-02-23 23:18:43 +0300

    fix labels and add ru_locale

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module AskMe
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.default_locale = :ru
   end
 end
--- db/migrate/20220219190056_add_default_color_to_user.rb
+++ db/migrate/20220219190056_add_default_color_to_user.rb
@@ -1,5 +1,5 @@
 class AddDefaultColorToUser < ActiveRecord::Migration[6.1]
   def change
-    change_column :users, :color, :string, :default => "#005a55"
+    change_column :users, :color, :string, default: "#005a55"
   end
 end
